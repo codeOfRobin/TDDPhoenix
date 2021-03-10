c3aeac0414631098e179dd5f8476795c7d7aa84b
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-03-10 02:19:28 +0000

    stuff

--- chatter/config/dev.exs
+++ chatter/config/dev.exs
@@ -2,10 +2,11 @@ use Mix.Config
 
 # Configure your database
 config :chatter, Chatter.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "chatter_dev",
+  database: "chatter",
+  username: "test_user",
+  password: "workbox-rodeo-forecast-rectify-taxiderm",
   hostname: "localhost",
+  port: "5432",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
--- chatter/config/test.exs
+++ chatter/config/test.exs
@@ -6,17 +6,19 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :chatter, Chatter.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "chatter_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "chatter",
+  username: "test_user",
+  password: "workbox-rodeo-forecast-rectify-taxiderm",
   hostname: "localhost",
+  port: "5432",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :chatter, ChatterWeb.Endpoint,
   http: [port: 4002],
-  server: false
+  server: true
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+config :chatter, :sql_sandbox, true
