feb4349122194ac2ad98373be6dfb57c2d5b360a
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-04-25 23:34:55 +0100

    Moving to JS land

--- chatter/assets/js/app.js
+++ chatter/assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "../css/app.scss";
+import socket from "./socket";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,7 +10,10 @@ import "../css/app.scss"
 //
 // Import deps with the dep name or local files with a relative path, for example:
 //
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
-//
-import "phoenix_html"
+let socket = new Socket("/socket", { params: {} });
+
+let chatRoomTitle = document.getElementById("title");
+let chatRoomName = chatRoomTitle.dataset.chatRoomName;
+let channel = socket.channel(`chat_room:${chatRoomName}`, {});
+
+import "phoenix_html";
