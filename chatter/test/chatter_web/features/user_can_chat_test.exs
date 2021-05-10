7c7b360371c19843470d0d70aac7b6c40b823299
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-05-10 22:31:29 +0100

    fix tests

--- chatter/test/chatter_web/features/user_can_chat_test.exs
+++ chatter/test/chatter_web/features/user_can_chat_test.exs
@@ -9,14 +9,14 @@ defmodule ChatterWeb.UserCanChatTest do
 
     session1 =
       metadata
-      |> new_user()
+      |> new_session()
       |> visit(rooms_index())
       |> sign_in(as: user1)
       |> join_room(room.name)
 
     session2 =
       metadata
-      |> new_user()
+      |> new_session()
       |> visit(rooms_index())
       |> sign_in(as: user2)
       |> join_room(room.name)
@@ -32,6 +32,26 @@ defmodule ChatterWeb.UserCanChatTest do
     |> assert_has(message("Hi, welcome to #{room.name}", author: user2))
   end
 
+  test "new user can see previous messages in chat history", %{metadata: metadata} do
+    room = insert(:chat_room)
+    user1 = insert(:user)
+    user2 = insert(:user)
+
+    metadata
+    |> new_session()
+    |> visit(rooms_index())
+    |> sign_in(as: user1)
+    |> join_room(room.name)
+    |> add_message("Welcome future users")
+
+    metadata
+    |> new_session()
+    |> visit(rooms_index())
+    |> sign_in(as: user2)
+    |> join_room(room.name)
+    |> assert_has(message("Welcome future users", author: user1))
+  end
+
   defp message(text, author: author) do
     message = "#{author.email}: #{text}"
     Query.data("role", "message", text: message)
@@ -43,9 +63,9 @@ defmodule ChatterWeb.UserCanChatTest do
     |> click(Query.button("Send"))
   end
 
-  defp new_user(metadata) do
-    {:ok, user} = Wallaby.start_session(metadata: metadata)
-    user
+  defp new_session(metadata) do
+    {:ok, session} = Wallaby.start_session(metadata: metadata)
+    session
   end
 
   defp rooms_index(), do: Routes.chat_room_path(@endpoint, :index)
